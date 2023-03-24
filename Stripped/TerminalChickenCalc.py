6b5996197a5f74c1763aab5b21c7de9017695fb8
Albert Mathisz <albertmathisz0@gmail.com>
2023-03-24 09:51:29 -0400

    Added Stuff

---
+++ Stripped/TerminalChickenCalc.py
@@ -0,0 +1,9 @@
+import math
+
+print("Input your age:")
+
+age = input()
+
+chickensEaten = int(age) * 33.80 #Got the stats from an international study on chicken consumption
+
+print("You have eaten approximately" + " " + str(round(chickensEaten)) + " " + "chickens in your life!")
